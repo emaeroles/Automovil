f58eaf03104930e6165597264e7bda8b2d606558
Emanuel Eroles <emanueleroles@gmail.com>
2021-04-17 14:38:18 -0300

    Agregar archivos de proyecto.

---
+++ Automovil/Auto.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace Automovil
+{
+    class Auto
+    {
+        private float kilometros;
+        private float combustible;
+        private float tanque;
+        private readonly byte km_l;
+        private bool reserva;
+        public Auto()
+        {
+            kilometros = 1f;
+            combustible = 1f;
+            tanque = 0f;
+            km_l = 11;
+            reserva = false;
+        }
+        public float Kilometros
+        {
+            set
+            {
+                kilometros = value;
+            }
+            get
+            {
+                return kilometros;
+            }
+        }
+        public float Combustible
+        {
+            set
+            {
+                combustible = value;
+            }
+            get
+            {
+                return combustible;
+            }
+        }
+        public string Conducir()  // Método para disminuir el valor de la variable tanque 
+        {                         // si kilometros no supera 594 y si kmPuede es >= que kilometros
+            float kmPuede = km_l * tanque;
+
+            kmPuede = (float)Math.Round(kmPuede * 1000f) / 1000f;
+
+            if (tanque - kilometros / km_l <= 5)
+            {
+                reserva = true;
+            }
+
+            if (kilometros > 594 && tanque > 0)
+            {
+                return "No puede recorre " + kilometros + "km, excede las capacidades del vehículo, usted solo puede recorrer " + kmPuede + "km";
+            }
+            else if (tanque == 0)
+            {
+                return "No hay combustible en el tanque, no puede conducir";
+            }
+            else if (tanque > 0 && tanque <= 5)
+            {
+                if (kmPuede >= kilometros)
+                {
+                    tanque -= kilometros / km_l;
+                    tanque = (float)Math.Round(tanque * 1000f) / 1000f;
+                    return "Puede recorrer " + kilometros + "km, pero cuidado que esta en la reserva";
+                }
+                else
+                {
+                    return "No puede recorre " + kilometros + " km, usted solo puede recorrer " + kmPuede + "km";
+                }
+            }
+            else
+            {
+                if (kmPuede >= kilometros)
+                {
+                    if (reserva == true)
+                    {
+                        tanque -= kilometros / km_l;
+                        tanque = (float)Math.Round(tanque * 1000f) / 1000f;
+                        return "Puede recorrer " + kilometros + "km, pero cuidado que quedo en la reserva";
+                    }
+                    else
+                    {
+                        tanque -= kilometros / km_l;
+                        tanque = (float)Math.Round(tanque * 1000f) / 1000f;
+                        return "Puede recorrer " + kilometros + "km, sin problemas";
+                    }
+                }
+                else
+                {
+                    return "No puede recorre " + kilometros + "km, usted solo puede recorrer " + kmPuede + "km";
+                }
+            }
+        }
+
+        public string Cargar()  // Método para aumentar el valor de la variable tanque
+        {                       // si el tanque no esta lleno
+            string salida;
+            float derrame;
+
+            if (tanque < 54)
+            {
+                if (54 - tanque >= combustible)
+                {
+                    tanque += combustible;
+                    salida = "Se ha cargado combustible";
+                }
+                else
+                {
+                    derrame = combustible - (54 - tanque);
+                    derrame = (float)Math.Round(derrame * 100f) / 100f;
+                    tanque = 54;
+                    salida = "El tanque se lleno y se derramaron " + derrame + " litros de combustible";
+                }
+            }
+            else
+            {
+                salida = "El tanque esta lleno";
+            }
+
+            if (tanque > 5)
+            {
+                reserva = false;
+            }
+            return salida;
+        }
+    }
+}
---
+++ Automovil/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Automovil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto auto = new Auto();
+            byte bandera = 0;
+            string cadena;
+
+            void titulo()
+            {
+                Console.WriteLine("");
+                Console.WriteLine("   ---------------------------------------------------");
+                Console.WriteLine("   |  Automóvil: conduciendo y cargando combustible  |");
+                Console.WriteLine("   ---------------------------------------------------");
+                Console.WriteLine("");
+            }
+
+            titulo();
+
+            do
+            {
+                Console.WriteLine("Desea conducir (1), cargar el tanque (2) o salir del programa (0)");
+                do
+                {
+                    if (bandera != 0 && bandera != 1 && bandera != 2)
+                    {
+                        Console.WriteLine("Ingrese un código correcto: conducir (1), cargar el tanque (2) o salir del programa (0)");
+                    }
+                    try
+                    {
+                        bandera = byte.Parse(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        bandera = 3;
+                    }
+                    Console.Clear();
+                    titulo();
+                } while (bandera != 0 && bandera != 1 && bandera != 2);
+
+                if (bandera == 1)
+                {
+                    Console.WriteLine("Ingresar los km que quiere recorrer");
+                    do  // bucle para tomar un valor correcto en kilometros
+                    {
+                        if (auto.Kilometros <= 0)
+                        {
+                            Console.WriteLine("Ingresar los km que quiere recorrer, ingrese un valor correcto");
+                        }
+                        try
+                        {
+                            cadena = Console.ReadLine();
+                            auto.Kilometros = float.Parse(cadena.Replace(".", ","));
+                        }
+                        catch
+                        {
+                            auto.Kilometros = -1;
+                        }
+                        Console.Clear();
+                        titulo();
+                    } while (auto.Kilometros <= 0);
+                    Console.WriteLine(auto.Conducir());  // el método se ejecuta cuando hay un valor correcto en kilometros
+                    Console.WriteLine("");
+                }
+
+                if (bandera == 2)
+                {
+                    Console.WriteLine("Ingresar los litros que quiere cargar");
+                    do  // bucle para tomar un valor correcto en combustible
+                    {
+                        if (auto.Combustible <= 0)
+                        {
+                            Console.WriteLine("Ingresar los litro que quiere cargar, ingrese un valor correcto");
+                        }
+                        try
+                        {
+                            cadena = Console.ReadLine();
+                            auto.Combustible = float.Parse(cadena.Replace(".", ","));
+                        }
+                        catch
+                        {
+                            auto.Combustible = -1;
+                        }
+                        Console.Clear();
+                        titulo();
+                    } while (auto.Combustible <= 0);
+                    Console.WriteLine(auto.Cargar());  // el método se ejecuta cuando hay un valor correcto en combustible
+                    Console.WriteLine("");
+                }
+            } while (bandera != 0);
+        }
+    }
+}
